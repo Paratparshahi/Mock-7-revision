297042eccda71c78101068c967a4900993403e38
Pratik Kumar <khoonipratik@gmail.com>
2023-01-09 23:10:56 +0530

    added

--- Back/server.js
+++
@@ -1,9 +0,0 @@
-const Jsonserver= require('json-server');
-const server = Jsonserver.create();
-const router = Jsonserver.router('db.json');
-const middleware=Jsonserver.defaults();
-const port= process.env.PORT||7000;
-
-server.use(middleware);
-server.use(router);
-server.listen(port)
